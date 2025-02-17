fa019cad13ee709a257989ecc4266248376278a9
Rob Coleman <rccoleman@gmail.com>
2025-02-16 19:44:34 -0800

    Update media_player.py

--- esphome/components/nabu/media_player.py
+++ esphome/components/nabu/media_player.py
@@ -214,7 +214,7 @@ FINAL_VALIDATE_SCHEMA = _supported_local_file_validate
 
 
 async def to_code(config):
-    cg.add_library("esphome/esp-audio-libs", "1.0.0")
+    cg.add_library("esphome/esp-audio-libs", "1.1.1")
 
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
